621a0d74d0d3b3c6205aa2eac17d2f0a9af3f8ed
Alicia Key <alicia@akey7.com>
2020-03-28 20:07:29 -0600

    Issue #1
    Make to CSV outputs

--- parse_energies.py
+++ parse_energies.py
@@ -7,22 +7,42 @@ from sys import stdout
 
 import pandas as pd
 
-# TS           BOND          ANGLE          DIHED          IMPRP               ELECT            VDW
+energies_by_ts_and_type = []
+energies_by_ts = []
 
-rows = []
 for line in fileinput.input():
     if line.startswith("ENERGY:"):
         values = [m for m in [l.strip() for l in line.split(" ")][1:] if len(m) > 0]
-        row = {
-            "timestep": int(values[0]),
+
+        timestep = int(values[0])
+
+        energies_by_ts.append({
+            "timestep": timestep,
             "bond [kcal/mol]": float(values[1]),
             "angle [kcal/mol]": float(values[2]),
             "dihedral [kcal/mol]": float(values[3]),
             "improper [kcal/mol]": float(values[4]),
             "electrostatic [kcal/mol]": float(values[5]),
             "VDW [kcal/mol]": float(values[6])
-        }
-        rows.append(row)
+        })
 
-df = pd.DataFrame(rows)
-df.to_csv(stdout, index=False)
+        columns = {}
+        columns["bond"] = float(values[1])
+        columns["angle"] = float(values[2])
+        columns["dihedral"] = float(values[3])
+        columns["improper"] = float(values[4])
+        columns["electrostatic"] = float(values[5])
+        columns["vdw"] = float(values[6])
+
+        for energy, kcal_mol in columns.items():
+            energies_by_ts_and_type.append({
+                "timestep": timestep,
+                "energy": energy,
+                "kcal/mol": kcal_mol
+            })
+
+energies_by_ts_and_type_df = pd.DataFrame(energies_by_ts_and_type)
+energies_by_ts_df = pd.DataFrame(energies_by_ts)
+
+energies_by_ts_df.to_csv("energies_by_ts.csv", index=False)
+energies_by_ts_and_type_df.to_csv("energies_by_ts_and_type.csv", index=True)
