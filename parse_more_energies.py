25e03d51fef07b4c07eef91edd5c680e47bcd257
Alicia Key <alicia@akey7.com>
2020-03-29 10:49:02 -0600

    Issue #1
    Almost there with the new parsing.

--- parse_more_energies.py
+++ parse_more_energies.py
@@ -9,8 +9,8 @@ import pandas as pd
 # An explanation for the units can be found at:
 # https://www.ks.uiuc.edu/Training/Tutorials/namd/namd-tutorial-html/node27.html
 
-# TS,BOND kcal/mol,ANGLE kcal/mol,DIHED kcal/mol,ANGLE kcal/mol,IMPRP kcal/mol,ELECT kcal/mol,VDW kcal/mol,BOUNDARY kcal/mol,MISC kcal/mol,KINETIC kcal/mol,TOTAL kcal/mol,TEMP K,     POTENTIAL kcal/mol,TOTAL3 kcal/mol,TEMPAVG K, PRESSURE bar, GPRESSURE bar, VOLUME Å^3, PRESSAVG bar,GPRESSAVG bar
-# 0, 2060.4925,    1271.6255,     263.8635,      10.7301,        -19408.9902,  2463.6381,     0.0000,      0.0000,           0.0000,       -13338.6406,     0.0000,        -13338.6406,-13338.6406,       0.0000,         -311.4071, 3996.8932,    86856.0000,    -311.4071,  3996.8932
+# These aren't all the values...they are the values that are I found on every line of
+# the log file.
 
 etitle = [
     "TS",
@@ -31,8 +31,7 @@ etitle = [
     "TEMPAVG K",
     "PRESSURE bar",
     "GPRESSURE bar",
-    "VOLUME Å^3",
-    "PRESSAVG bar"
+    "VOLUME Å^3"
 ]
 
 energies_and_temps_wide = []
@@ -41,15 +40,20 @@ energies_and_temps_tall = []
 for line in fileinput.input():
     if line.startswith("ENERGY:"):
         values = [m for m in [l.strip() for l in line.strip().split(" ")][1:] if len(m) > 0]
-        timestep = int(values[0])
-        print(",".join(etitle))
-        print(",".join(values))
         row = {}
         for i, key in enumerate(etitle):
-            row[key] = values[i]
+            row[key] = float(values[i])
         energies_and_temps_wide.append(row)
+        for key, value in row.items():
+            if key != "TS":
+                energies_and_temps_tall.append({
+                    "timestep": int(values[0]),
+                    key: value
+                })
 
 wide_df = pd.DataFrame(energies_and_temps_wide)
+tall_df = pd.DataFrame(energies_and_temps_tall)
 
 timestamp = round(datetime.datetime.utcnow().timestamp())
-wide_df.to_csv(f"namd_log_{timestamp}.csv", index=False)
+wide_df.to_csv(f"namd_log_{timestamp}.wide.csv", index=False)
+tall_df.to_csv(f"namd_log_{timestamp}.tall.csv", index=True)
